47f9065da8d2a2ae3ae9311d90b0e98971129757
adubey230 <dubeya2@uci.edu>
2026-01-17 11:34:47 -0800

    player animations

--- FineAuzz/Assets/scripts/Player.cs
+++ FineAuzz/Assets/scripts/Player.cs
@@ -4,6 +4,7 @@ public class Player : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] Animator animator;
+    [SerializeField] SpriteRenderer sprite;
     public float speed = 5f;
     private bool playerIsMoving = false;
     private Rigidbody2D rb; 
@@ -23,11 +24,43 @@ public class Player : MonoBehaviour
 
         rb.linearVelocity = movement * speed;
 
-        animator.SetBool("is_walk_front", movement.magnitude > 0);
+        //animator.SetBool("is_walk_front", movement.magnitude > 0);
+        Animations();
+    }
+
+    private void ResetAnimations()
+    {
+        animator.SetBool("is_walk_front", false);
+        animator.SetBool("is_back_walk", false);
+        animator.SetBool("is_side_walk", false);
+        animator.SetBool("is_idle", false);
+        sprite.flipX = false;
     }
 
     private void Animations()
     {
-       
+        ResetAnimations();
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            animator.SetBool("is_walk_front", true);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetBool("is_back_walk", true);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            animator.SetBool("is_side_walk", true);
+            sprite.flipX = true;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            animator.SetBool("is_side_walk", true);
+        }
+        else
+        {
+            animator.SetBool("is_idle", true);
+        }
     }
 }
