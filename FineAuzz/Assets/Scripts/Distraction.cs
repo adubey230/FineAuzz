abd2b72f37d45c4349eaf8aaeb56ca5124517c00
David-Li372 <138244633+David-Li372@users.noreply.github.com>
2026-01-17 14:22:42 -0800

    tft time

--- FineAuzz/Assets/Scripts/Distraction.cs
+++ FineAuzz/Assets/Scripts/Distraction.cs
@@ -5,45 +5,23 @@ public class Distraction : MonoBehaviour
 {   
     public bool IsCracked = false;
 
-    private bool InRange = false;
-
-    private Collider2D vase;
-
-    void Start(){
-        vase = GetComponent<Collider2D>();
-    }
-
     [SerializeField] public Sprite broken;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start(){
+        
+    }
 
-    
     void Update(){
         if(Input.GetKeyDown(KeyCode.F)){
             CrackVase();
         }
     }
 
-    private void CrackVase(){
-        if(!IsCracked && InRange){
+    public void CrackVase(){
+        if(!IsCracked){
             Debug.Log("crack");
             IsCracked = true;
             GetComponent<SpriteRenderer>().sprite = broken;
-            //LookAtDist(transform.position.x,transform.position.y);
-        }
-    }
-    
-    void OnTriggerEnter2D(Collider2D collider)
-    {   
-        Debug.Log("enter");
-        if(collider.tag == "Range"){
-            InRange = true;
-        }
-    }
-
-    void OnTriggerExit2D(Collider2D collider)
-    {
-        if(collider.tag == "Range"){
-            InRange = false;
         }
     }
 }
--- FineAuzz/Assets/scripts/GameManager.cs
+++ FineAuzz/Assets/scripts/GameManager.cs
@@ -8,22 +8,30 @@ public class GameManager : MonoBehaviour
     [SerializeField] private int timeToReset = 120;
     private bool startResetTimer = false;
     private Player player;
+    private GameObject[] guards;
+    [SerializeField] Distraction vase;
 
     private string currentScene;
 
     private void OnEnable()
     {
         GuardLOS.PlayerDetected += HandlePlayerCaught;
+        Player.DestroyVase += TriggerVaseActions;
     }
+
     private void OnDisable()
     {
         GuardLOS.PlayerDetected -= HandlePlayerCaught;
+        Player.DestroyVase += TriggerVaseActions;
     }
+
     void Start()
     {
         currentScene = SceneManager.GetActiveScene().name;
         player = FindFirstObjectByType<Player>();
-}
+        guards = GameObject.FindGameObjectsWithTag("Enemy");;
+    }
+
     void FixedUpdate()
     {
         if (startResetTimer)
@@ -42,4 +50,10 @@ public class GameManager : MonoBehaviour
         startResetTimer = true;
         player.Die();
     }
+
+    private void TriggerVaseActions(Distraction vase){
+        vase.CrackVase();
+        
+    }
+
 }
