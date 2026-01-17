aa78226248d019f77dd16359ac6a87a0b4a12a47
adubey230 <dubeya2@uci.edu>
2026-01-17 11:58:44 -0800

    cc

--- FineAuzz/Assets/Scripts/Distraction.cs
+++ FineAuzz/Assets/Scripts/Distraction.cs
@@ -1,43 +1,43 @@
 
-// using UnityEngine;
+using UnityEngine;
 
-// public class Distraction : MonoBehaviour
-// {   
-//     public bool IsCracked = false;
+public class Distraction : MonoBehaviour
+{   
+    public bool IsCracked = false;
 
-//     private Collider2D vase;
+    private Collider2D vase;
 
-//     void Start(){
-//         vase = GetComponent<Collider2D>();
-//     }
+    void Start(){
+        vase = GetComponent<Collider2D>();
+    }
 
-//     [SerializeField] public Sprite broken;
-//     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] public Sprite broken;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-//     public bool canInteract(){
-//         return !IsCracked && InRange();
-//     }
+    public bool canInteract(){
+        return !IsCracked && InRange();
+    }
     
-//     public bool InRange(){
-//         vase.OnTriggerStay2D(Collider2D collider){
-//             if(collider.tag == "range"){
-//                 return true;
-//             }
-//         }
-//         return false;
-//     }
+    public bool InRange(){
+        vase.OnTriggerStay2D(Collider2D collider){
+            if(collider.tag == "range"){
+                return true;
+            }
+        }
+        return false;
+    }
 
-//     public void Interact(){
-//         if(!canInteract()){
-//             return;
-//         }
-//         CrackVase();
-//     }
+    public void Interact(){
+        if(!canInteract()){
+            return;
+        }
+        CrackVase();
+    }
 
-//     private void CrackVase(){
-//         IsCracked = true;
-//         GetComponent<SpriteRenderer>().sprite = broken;
-//         //LookAtDist(transform.position.x,transform.position.y);
+    private void CrackVase(){
+        IsCracked = true;
+        GetComponent<SpriteRenderer>().sprite = broken;
+        //LookAtDist(transform.position.x,transform.position.y);
         
-//     }
-// }
+    }
+}
