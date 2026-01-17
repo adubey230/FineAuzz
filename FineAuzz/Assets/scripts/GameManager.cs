613e5893fcf71810e3d12fc0ee9cbd89a98e6c6a
KrazyJax <jacksongranberg@gmail.com>
2026-01-16 19:59:33 -0800

    Reapply "Merge branch 'main' of https://github.com/adubey230/FineAuzz"
    This reverts commit 4b0c98d01fd13795889057e1ec5908dcfef844c3.

---
+++ FineAuzz/Assets/scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject exit;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
