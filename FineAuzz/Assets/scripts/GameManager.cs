f215398173c8102caa269ac8e1d91c4f538b1c6c
adubey230 <dubeya2@uci.edu>
2026-01-16 19:24:57 -0800

    player movement

---
+++ FineAuzz/Assets/scripts/GameManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform targetPosition;
+    [SerializeField] GameObject exit1;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
