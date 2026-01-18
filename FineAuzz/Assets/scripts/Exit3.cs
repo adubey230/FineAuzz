96d5118d957dc901f043e41617db6571d4ff9017
adubey230 <dubeya2@uci.edu>
2026-01-18 13:50:21 -0800

    room 5 fixed

---
+++ FineAuzz/Assets/scripts/Exit3.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Exit3 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Debug.Log("Hello");
+        if(other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Room3");
+        }
+    }
+}
