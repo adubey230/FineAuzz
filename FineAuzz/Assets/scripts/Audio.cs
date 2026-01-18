cab6a6b544910dd97c48219b187a6741019e8f7a
adubey230 <dubeya2@uci.edu>
2026-01-18 13:31:31 -0800

    audio

--- FineAuzz/Assets/scripts/Audio.cs
+++ FineAuzz/Assets/scripts/Audio.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Alarms : MonoBehaviour
+public class Audio : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] AudioSource source;
