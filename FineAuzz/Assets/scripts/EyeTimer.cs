8b9ca85d43e9091304fd891e6d2ddf10c7f161d7
KrazyJax <jacksongranberg@gmail.com>
2026-01-18 12:12:26 -0800

    updates

--- FineAuzz/Assets/scripts/EyeTimer.cs
+++ FineAuzz/Assets/scripts/EyeTimer.cs
@@ -10,8 +10,10 @@ public class EyeTimer : MonoBehaviour
     [SerializeField] private Sprite OpenEye;
     [SerializeField] private Sprite ClosedEye;
     [SerializeField] private GameObject guard;
+    [SerializeField] private CanvasScaler scaler;
     public float blinkDuration;
     public float resetDuration;
+
     [SerializeField] public float offset;
     public Camera cam;
     private float remainingDur;
@@ -32,11 +34,11 @@ public class EyeTimer : MonoBehaviour
 
     void Update(){
 
+
         Vector3 screenPos = cam.WorldToScreenPoint(guard.transform.position);
-        Vector3 finalPos = new Vector3(screenPos.x, screenPos.y + offset, 0);
+        Vector3 finalPos = new Vector3(screenPos.x, screenPos.y + offset * scaler.scaleFactor, 0);
         // If using RectTransform:
         GetComponent<RectTransform>().position = finalPos;
-
 
         //transform.position = cam.ScreenToWorldPoint(new Vector3(guard.transform.position.x, guard.transform.position.y ));
         
