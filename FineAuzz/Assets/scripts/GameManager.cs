f8dd31eb993a422fcf905d6dbe5b2e9374d483cb
David-Li372 <138244633+David-Li372@users.noreply.github.com>
2026-01-17 23:57:57 -0800

    ITS ALIVE

--- FineAuzz/Assets/scripts/GameManager.cs
+++ FineAuzz/Assets/scripts/GameManager.cs
@@ -24,7 +24,7 @@ public class GameManager : MonoBehaviour
     private void OnDisable()
     {
         GuardLOS.PlayerDetected -= HandlePlayerCaught;
-        Player.DestroyVase += TriggerVaseActions;
+        Player.DestroyVase -= TriggerVaseActions;
     }
 
     void Start()
