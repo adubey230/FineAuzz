9bf046a95a7fa477364decff807ef3971355aadf
adubey230 <dubeya2@uci.edu>
2026-01-18 14:11:15 -0800

    new audio

--- FineAuzz/Assets/scripts/Alarms.cs
+++ FineAuzz/Assets/scripts/Alarms.cs
@@ -5,10 +5,14 @@ public class Alarms : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] AudioSource source;
     [SerializeField] AudioClip clip;
+    [SerializeField] Audio1 bgAud;
+    [SerializeField] AudioClip newClip;
     void Start()
     {
         source.clip = clip;
         source.Play();
+        bgAud.source.clip = newClip;
+        bgAud.source.Play();
     }
 
     // Update is called once per frame
